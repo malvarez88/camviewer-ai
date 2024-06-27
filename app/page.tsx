ff35040759307a273f4219c240ee6ebc3108b01f
mariano alvarez <mariano@codingco.io>
2024-06-27 12:57:54 -0300

    feat: add cocossd ai model detection

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,272 @@
-import React from "react";
+"use client";
+import { ModeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@/components/ui/popover";
+import { Separator } from "@/components/ui/separator";
+import React, { useEffect, useRef, useState } from "react";
+import { Rings } from "react-loader-spinner";
+import Webcam from "react-webcam";
+import { toast } from "sonner";
+import {
+  Camera,
+  Divide,
+  FlipHorizontal,
+  MoonIcon,
+  PersonStanding,
+  SunIcon,
+  Video,
+  Volume2,
+} from "lucide-react";
+import { Slider } from "@/components/ui/slider";
+import { beep } from "@/utils/beep";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs-backend-cpu";
+import "@tensorflow/tfjs-backend-webgl";
+import { ObjectDetection } from "@tensorflow-models/coco-ssd";
 
 type Props = {};
 
 const HomePage = (props: Props) => {
-  return <div>HomePage</div>;
+  const webcamRef = useRef<Webcam>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [mirrored, setMirrored] = useState<boolean>(true);
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+  const [autoRecordEnabled, setAutoRecordEnabled] = useState<boolean>(false);
+  const [volume, setVolume] = useState(0.8);
+  const [model, setModel] = useState<ObjectDetection>();
+
+  function userPromptScreenshot(event: any) {
+    throw new Error("Function not implemented.");
+  }
+
+  function userPromptRecord(event: any) {
+    throw new Error("Function not implemented.");
+  }
+
+  function toggleAutoRecord(event: any) {
+    if (autoRecordEnabled) {
+      setAutoRecordEnabled(false);
+      toast("Autorecord disabled.", {
+        // description: "Sunday, December 03, 2023 at 9:00 AM",
+      });
+    } else {
+      setAutoRecordEnabled(true);
+      toast("Autorecord enabled.", {
+        // description: "Sunday, December 03, 2023 at 9:00 AM",
+      });
+    }
+  }
+
+  useEffect(() => {
+    initModel();
+  }, []);
+
+  //load ai model && set a state variable
+  async function initModel() {
+    const loadModel: ObjectDetection = await cocossd.load({
+      base: "mobilenet_v2",
+    });
+    setModel(loadModel);
+  }
+
+  return (
+    <div className="flex h-screen">
+      {/* left - webcam */}
+      <div className="relative">
+        <div className="relative h-screen w-full">
+          <Webcam
+            ref={webcamRef}
+            mirrored={mirrored}
+            className="h-full w-full object-contain p-2"
+          />
+          <canvas
+            ref={canvasRef}
+            className="absolute top-0 left-0 h-full w-full object-contain"
+          ></canvas>
+        </div>
+      </div>
+      {/* right - buttons */}
+      <div className="flex flex-row flex-1">
+        <div className="border-primary/5 border-2 max-w-xs flex flex-col gap-2 justify-between shadow-md rounded-md p-4">
+          {/* top */}
+          <div className="flex flex-col gap-2">
+            <ModeToggle />
+            <Button
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal />
+            </Button>
+            <Separator className="my-2" />
+          </div>
+          {/* middle */}
+          <div className="flex flex-col gap-2">
+            <Separator className="my-2" />
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              onClick={userPromptScreenshot}
+              size={"icon"}
+            >
+              <Camera />
+            </Button>
+            <Button
+              variant={isRecording ? "destructive" : "outline"}
+              onClick={userPromptRecord}
+              size={"icon"}
+            >
+              <Video />
+            </Button>
+            <Separator className="my-2" />
+            <Button
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={40} width={40} />
+              ) : (
+                <PersonStanding />
+              )}
+            </Button>
+          </div>
+
+          {/* bottom */}
+          <div className="flex flex-col gap-2">
+            <Separator className="my-2" />
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant={"outline"} size={"icon"}>
+                  <Volume2 />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent>
+                <Slider
+                  max={1}
+                  min={0}
+                  step={0.2}
+                  defaultValue={[volume]}
+                  onValueCommit={(val) => {
+                    setVolume(val[0]);
+                    beep(val[0]);
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+        <div className="h-full flex-1 py-4 px-2 overflow-y-scroll">
+          <RenderFeatureHighlightsSection />
+        </div>
+      </div>
+    </div>
+  );
+
+  function RenderFeatureHighlightsSection() {
+    return (
+      <div className="text-xs text-muted-foreground">
+        <ul className="space-y-4">
+          <li>
+            <strong>Dark Mode/Sys Theme 🌗</strong>
+            <p>Toggle between dark mode and system theme.</p>
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <SunIcon size={14} />
+            </Button>{" "}
+            /{" "}
+            <Button className="my-2 h-6 w-6" variant={"outline"} size={"icon"}>
+              <MoonIcon size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Horizontal Flip ↔️</strong>
+            <p>Adjust horizontal orientation.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={() => {
+                setMirrored((prev) => !prev);
+              }}
+            >
+              <FlipHorizontal size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Take Pictures 📸</strong>
+            <p>Capture snapshots at any moment from the video feed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={"outline"}
+              size={"icon"}
+              onClick={userPromptScreenshot}
+            >
+              <Camera size={14} />
+            </Button>
+          </li>
+          <li>
+            <strong>Manual Video Recording 📽️</strong>
+            <p>Manually record video clips as needed.</p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={isRecording ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={userPromptRecord}
+            >
+              <Video size={14} />
+            </Button>
+          </li>
+          <Separator />
+          <li>
+            <strong>Enable/Disable Auto Record 🚫</strong>
+            <p>
+              Option to enable/disable automatic video recording whenever
+              required.
+            </p>
+            <Button
+              className="h-6 w-6 my-2"
+              variant={autoRecordEnabled ? "destructive" : "outline"}
+              size={"icon"}
+              onClick={toggleAutoRecord}
+            >
+              {autoRecordEnabled ? (
+                <Rings color="white" height={30} />
+              ) : (
+                <PersonStanding size={14} />
+              )}
+            </Button>
+          </li>
+
+          <li>
+            <strong>Volume Slider 🔊</strong>
+            <p>Adjust the volume level of the notifications.</p>
+          </li>
+          <li>
+            <strong>Camera Feed Highlighting 🎨</strong>
+            <p>
+              Highlights persons in{" "}
+              <span style={{ color: "#FF0F0F" }}>red</span> and other objects in{" "}
+              <span style={{ color: "#00B612" }}>green</span>.
+            </p>
+          </li>
+          <Separator />
+          <li className="space-y-4">
+            <strong>Share your thoughts 💬 </strong>
+            {/* <SocialMediaLinks /> */}
+            <br />
+            <br />
+            <br />
+          </li>
+        </ul>
+      </div>
+    );
+  }
 };
 
 export default HomePage;
